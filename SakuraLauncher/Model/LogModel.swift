7e3334727c8bc0b58d075566aaf415da42b0918d
fengberd <admin@berd.moe>
2021-06-15 17:49:17 +0800

    LogModel: update level handling

--- SakuraLauncher/Model/LogModel.swift
+++ SakuraLauncher/Model/LogModel.swift
@@ -7,30 +7,35 @@
 
 import SwiftUI
 
+enum LogLevel: String {
+    case info = "I",
+         warning = "W",
+         error = "E"
+    case none = ""
+}
+
 class LogModel: ObservableObject, Hashable {
-    static let infoColor = Color.white,
-               warningColor = Color.orange,
-               errorColor = Color(red: 0.86, green: 0.31, blue: 0.21)
-
-    static func == (lhs: LogModel, rhs: LogModel) -> Bool {
-        lhs.time == rhs.time &&
-            lhs.source == rhs.source &&
-            lhs.data == rhs.data &&
-            lhs.level == rhs.level
-    }
-
     var source: String
     var time: String
-    var level: String
+    var level: LogLevel
     var data: String
-    var levelColor: Color
 
-    internal init(source: String, time: String, level: String, data: String, levelColor: Color) {
+    internal init(source: String, time: String, level: LogLevel, data: String) {
         self.source = source
         self.time = time
         self.level = level
         self.data = data
-        self.levelColor = levelColor
+    }
+
+    func levelColor() -> Color {
+        switch level {
+        case .warning:
+            return Color.orange
+        case .error:
+            return Color(red: 0.86, green: 0.31, blue: 0.21)
+        default:
+            return Color.white
+        }
     }
 
     func hash(into hasher: inout Hasher) {
@@ -38,5 +43,12 @@ class LogModel: ObservableObject, Hashable {
         hasher.combine(time)
         hasher.combine(level)
         hasher.combine(data)
+    }
+
+    static func == (lhs: LogModel, rhs: LogModel) -> Bool {
+        lhs.time == rhs.time &&
+            lhs.source == rhs.source &&
+            lhs.data == rhs.data &&
+            lhs.level == rhs.level
     }
 }
