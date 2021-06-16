b1d7feb5e0868fe255daea426dbc013eb0243bcd
fengberd <admin@berd.moe>
2021-06-16 15:59:40 +0800

    SocketClient: Detect disconnect properly, fix invalid descriptor

--- SakuraLauncher/Helper/SocketClient.swift
+++ SakuraLauncher/Helper/SocketClient.swift
@@ -24,6 +24,7 @@ class SocketClient {
         self.path = path
         self.bufferSize = bufferSize
 
+        // We don't want to deal with tons of optional stuffs, so this creation is unavoidable
         mainConnection = try! Socket.create(family: .unix, type: .stream, proto: .unix)
         pushConnection = try! Socket.create(family: .unix, type: .stream, proto: .unix)
     }
@@ -34,10 +35,15 @@ class SocketClient {
     }
 
     func connect() -> Bool {
-        if mainConnection.isConnected, pushConnection.isConnected {
+        if connected {
             return true
         }
         do {
+            close()
+
+            mainConnection = try! Socket.create(family: .unix, type: .stream, proto: .unix)
+            pushConnection = try! Socket.create(family: .unix, type: .stream, proto: .unix)
+
             try mainConnection.connect(to: path + "service.sock")
             try pushConnection.connect(to: path + "service-push.sock")
 
@@ -58,7 +64,9 @@ class SocketClient {
 
             return mainConnection.isConnected && pushConnection.isConnected
         } catch let e {
+            // TODO: Should log this in a better way or discard it
             print(e)
+            close()
         }
         return false
     }
@@ -73,6 +81,7 @@ class SocketClient {
 
             return try readMessage(mainConnection, type: ResponseBase.self)
         } catch let e {
+            close()
             return ResponseBase.with {
                 $0.success = false
                 $0.message = "内部错误: \(e)"
@@ -87,8 +96,8 @@ class SocketClient {
     }
 
     private func readMessage<T: SwiftProtobuf.Message>(_ connection: Socket, type _: T.Type) throws -> T {
-        let size = try! readBytes(connection, count: 4)
-        return try T(serializedData: try! readBytes(connection, count: Int(size[0]) | (Int(size[1]) << 8) | (Int(size[2]) << 16) | (Int(size[3]) << 24)))
+        let size = try readBytes(connection, count: 4)
+        return try T(serializedData: try readBytes(connection, count: Int(size[0]) | (Int(size[1]) << 8) | (Int(size[2]) << 16) | (Int(size[3]) << 24)))
     }
 
     private func readBytes(_ connection: Socket, count: Int) throws -> Data {
@@ -98,11 +107,18 @@ class SocketClient {
         var i = count
 
         repeat {
-            let c = try! connection.read(into: p, bufSize: i, truncate: true)
+            let c = try connection.read(into: p, bufSize: i, truncate: true)
+            if c == 0 {
+                throw SocketError.closed
+            }
             p += c
             i -= c
         } while i > 0
 
         return Data(bytesNoCopy: buffer, count: count, deallocator: .free)
     }
+}
+
+enum SocketError: Error {
+    case closed
 }
