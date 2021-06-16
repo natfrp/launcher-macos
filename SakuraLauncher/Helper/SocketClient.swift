25ea64141744e268f01bea1752237d43f0f4ef37
fengberd <admin@berd.moe>
2021-06-16 12:53:45 +0800

    SocketClient: Don't block global queue. Also fixed a stupid mistake, I need some coffee

--- SakuraLauncher/Helper/SocketClient.swift
+++ SakuraLauncher/Helper/SocketClient.swift
@@ -13,25 +13,26 @@ import SwiftProtobuf
 class SocketClient {
     let path: String
     let bufferSize: Int
-    
+
+    var connected: Bool { mainConnection.isConnected && pushConnection.isConnected }
     var onPushMessage: ((PushMessageBase) -> Void)?
-    
-    var queue = DispatchQueue(label: "Socket Queue")
+
+    var queue = DispatchQueue(label: "Push Socket Queue", qos: .utility)
     var mainConnection, pushConnection: Socket
-    
-    init(_ path: String, bufferSize: Int = 1048576) {
+
+    init(_ path: String, bufferSize: Int = 1_048_576) {
         self.path = path
         self.bufferSize = bufferSize
-        
+
         mainConnection = try! Socket.create(family: .unix, type: .stream, proto: .unix)
         pushConnection = try! Socket.create(family: .unix, type: .stream, proto: .unix)
     }
-    
+
     func close() {
         mainConnection.close()
         pushConnection.close()
     }
-    
+
     func connect() -> Bool {
         if mainConnection.isConnected, pushConnection.isConnected {
             return true
@@ -39,8 +40,8 @@ class SocketClient {
         do {
             try mainConnection.connect(to: path + "service.sock")
             try pushConnection.connect(to: path + "service-push.sock")
-            
-            DispatchQueue.global(qos: .background).async { [self] in
+
+            queue.async { [self] in
                 do {
                     repeat {
                         let msg = try readMessage(pushConnection, type: PushMessageBase.self)
@@ -54,22 +55,22 @@ class SocketClient {
                     close()
                 }
             }
-            
+
             return mainConnection.isConnected && pushConnection.isConnected
         } catch let e {
             print(e)
         }
         return false
     }
-    
+
     func request(_ message: RequestBase) -> ResponseBase {
         do {
             let data = try message.serializedData()
             var count = UInt32(data.count)
-            
+
             try mainConnection.write(from: Data(bytes: &count, count: 4))
             try mainConnection.write(from: data)
-            
+
             return try readMessage(mainConnection, type: ResponseBase.self)
         } catch let e {
             return ResponseBase.with {
@@ -78,24 +79,30 @@ class SocketClient {
             }
         }
     }
-    
-    private func readMessage<T: SwiftProtobuf.Message>(_ connection: Socket, type: T.Type) throws -> T {
-        let size = try! readBytes(mainConnection, count: 4)
+
+    func request(_ id: MessageID) -> ResponseBase {
+        request(RequestBase.with {
+            $0.type = id
+        })
+    }
+
+    private func readMessage<T: SwiftProtobuf.Message>(_ connection: Socket, type _: T.Type) throws -> T {
+        let size = try! readBytes(connection, count: 4)
         return try T(serializedData: try! readBytes(connection, count: Int(size[0]) | (Int(size[1]) << 8) | (Int(size[2]) << 16) | (Int(size[3]) << 24)))
     }
-    
+
     private func readBytes(_ connection: Socket, count: Int) throws -> Data {
         let buffer = UnsafeMutablePointer<Int8>.allocate(capacity: count)
-        
+
         var p = buffer
         var i = count
-        
+
         repeat {
             let c = try! connection.read(into: p, bufSize: i, truncate: true)
             p += c
             i -= c
         } while i > 0
-        
+
         return Data(bytesNoCopy: buffer, count: count, deallocator: .free)
     }
 }
